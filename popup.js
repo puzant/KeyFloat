a6fef6313e1fb4f2a6a71f88ac542d7ba8fd24f3
Puzant <pbakjejian@hotmail.com>
2025-06-11 10:39:46 +0300

    add event listeners for toggle and sound input

--- popup.js
+++ popup.js
@@ -1,11 +1,39 @@
-document.getElementById("apply-btn").addEventListener("click", async () => {
-  const selectedLanguage = document.querySelector('input[name="language"]:checked').value
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.querySelector('.switch input')
+  const soundToggle = document.querySelector('.switch .sound-toggle')
 
-  await chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['content-script.js'],
+ chrome.storage.local.get(['keyboardEnabled', 'soundEnabled'], (result) => {
+    toggle.checked = result.keyboardEnabled || false
+    soundToggle.checked = result.soundEnabled || false
   })
 
-  chrome.tabs.sendMessage(tab.id, { lng: selectedLanguage })
+  soundToggle.addEventListener('change', async (e) => {
+    const isSoundChecked = e.target.checked
+    chrome.storage.local.set({ soundEnabled: isSoundChecked })
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+
+    chrome.tabs.sendMessage(tab.id, { isSoundEnabled: isSoundChecked })
+  })
+
+  toggle.addEventListener('change', async (e) => {
+    const isChecked = e.target.checked
+    chrome.storage.local.set({ keyboardEnabled: isChecked })
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+
+     await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['content-script.js'],
+      })
+      
+    if (isChecked) {
+      chrome.tabs.sendMessage(tab.id, { lng: 'ar' })
+      setTimeout(() => window.close(), 1000)
+    } else {
+      chrome.tabs.sendMessage(tab.id, { action: 'closeKeyboard' })
+    }
+
+    await chrome.storage.sync.set({ keyboardEnabled: e.target.checked })
+  })
+
 })
+
