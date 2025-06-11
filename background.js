cd0b2051c0360c66a0e10d1f021e40242b6ea303
Puzant <pbakjejian@hotmail.com>
2025-06-12 00:11:55 +0300

    implement servicer worker for messages handling

---
+++ background.js
@@ -0,0 +1,31 @@
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  if (!tab?.id) return;
+
+  switch (message.type) {
+    case "TOGGLE_SOUND":
+      chrome.tabs.sendMessage(tab.id, {
+        type: "SOUND_TOGGLE",
+        payload: { enabled: message.payload.enabled },
+      });
+      break;
+
+    case "TOGGLE_KEYBOARD":
+      if (message.payload.enabled) {
+        await chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          files: ["content-script.js"],
+        });
+
+        chrome.tabs.sendMessage(tab.id, {
+          type: "KEYBOARD_OPEN",
+          payload: { lng: "ar" },
+        });
+      } else {
+        chrome.tabs.sendMessage(tab.id, {
+          type: "KEYBOARD_CLOSE",
+        });
+      }
+      break;
+  }
+});
