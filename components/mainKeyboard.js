a5e0048bafebe000eced6da6e749e2001dcabd4f
Puzant <PBAKJEJIAN@hotmail.com>
2025-07-30 16:41:14 +0300

    add styles for keyboard wrapper and keyboard box

--- components/mainKeyboard.js
+++ components/mainKeyboard.js
@@ -10,9 +10,29 @@ export class MainKeyboard extends HTMLElement {
 
 	render() {
 		this.shadowRoot.innerHTML = `
+			<style>
+				#keyboard-wrapper {
+					width: 700px;
+					position: absolute;
+					top: 20px;
+					left: `${window.innerWidth - 700 - 20}px`;
+					z-index: 999999;
+					will-change: transform;
+				}
+
+				#keyboard-box {
+					background: var(--key-wrapper);
+					padding: 5px 10px;
+					border-radius: 8px;
+					z-index: 999999;
+				}
+			</style>
+
       <div id="keyboard-wrapper">
-        <div id="keyboard-header-slot"></div>
-        <div id="keyboard-rows-slot"></div>
+				<div id="keyboard-box">
+        	<div id="keyboard-header-slot"></div>
+        	<div id="keyboard-rows-slot"></div>
+				</div>
       </div>
 		`
 	}
