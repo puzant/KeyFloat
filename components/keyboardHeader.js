1865da622dd7de05d7cffc6996d24f7db50822b1
Puzant <PBAKJEJIAN@hotmail.com>
2025-07-30 16:41:32 +0300

    implement structure + styling

--- components/keyboardHeader.js
+++ components/keyboardHeader.js
@@ -2,10 +2,6 @@ export class KeyboardHeader extends HTMLElement {
 	constructor() {
 		super()
 		this.attachShadow({ mode: 'open' }).appendChild(template.content.cloneNode(true))
-	}
-
-	static get observedAttributes() {
-		return ['language', 'theme']
 	}	
 }
 
