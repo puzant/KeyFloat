4b23eee62991a8fb21aaf2c701e5b60c9f20c806
Puzant <PBAKJEJIAN@hotmail.com>
2025-07-30 16:43:53 +0300

    prepare HTML structure for keyboard header element

--- components/keyboardHeader.js
+++ components/keyboardHeader.js
@@ -3,5 +3,27 @@ export class KeyboardHeader extends HTMLElement {
 		super()
 		this.attachShadow({ mode: 'open' }).appendChild(template.content.cloneNode(true))
 	}	
+
+	connectedCallback() {
+		this.render()
+	}
+
+	render() {
+		this.shadowRoot.innerHTML = `
+			<style>
+			
+			</style>
+
+			<div>
+				<span style="font-size: 18px;">${getLanguageName(language)}</span> 
+				
+				<div style="display: flex; gap: 12px;">
+        	<img id="toggle-theme" style="cursor: pointer" src=${chrome.runtime.getURL("assets/moon-icon.svg")} />
+        	<img id="shutdown-btn" style="cursor: pointer" src=${chrome.runtime.getURL("assets/shutdown-icon-dark.svg")} />
+        	<img id="collapse-btn" style="cursor: pointer" src=${chrome.runtime.getURL("assets/collapse-icon-dark.svg")} />
+      	</div>
+			</div>
+		`
+	}
 }
 
