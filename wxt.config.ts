25c8ec04a6f5e9740ae0a02d2990fc58e1138f01
Puzant <pbakjejian@hotmail.com>
2026-02-06 14:17:42 +0300

    add info for manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
-    permissions: ['storage', 'scripting', 'tabs']
+    permissions: ['storage', 'scripting', 'tabs'],
+    version: '1.4.0',
+    name: 'KeyFloat',
+    description: 'Floating multilingual keyboard with native key mappings, drag, dark mode, sounds, and dynamic layouts for macOS & Windows',
+    host_permissions: ['<all_urls>'],    
   }
 });
