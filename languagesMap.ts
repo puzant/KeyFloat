773ff34ddb85c20f10b3b8eb635b2d50e7c39816
Puzant <pbakjejian@hotmail.com>
2026-03-11 18:32:47 +0300

    update languages map

--- languagesMap.ts
+++ languagesMap.ts
@@ -1,114 +1,114 @@
 export const numbersMacLayout = [
-  { num: "`", ar: [], fr: ["#"], sp: [], am: [""], eventName: "Backquote" },
-  { num: "1", ar: [], fr: ["&"], sp: [], am: ["։"], eventName: "Digit1" },
-  { num: "2", ar: [], fr: ["é"], sp: [], am: ["ձ"], eventName: "Digit2" },
-  { num: "3", ar: [], fr: [""], sp: [], am: ["յ"], eventName: "Digit3" },
-  { num: "4", ar: [], fr: ["'"], sp: [], am: ["՛"], eventName: "Digit4" },
-  { num: "5", ar: [], fr: [""], sp: [], am: [","], eventName: "Digit5" },
-  { num: "6", ar: [], fr: ["§"], sp: [], am: ["-"], eventName: "Digit6" },
-  { num: "7", ar: [], fr: ["è"], sp: [], am: ["."], eventName: "Digit7" },
-  { num: "8", ar: [], fr: ["!"], sp: [], am: ["«"], eventName: "Digit8" },
-  { num: "9", ar: [], fr: ["ç"], sp: [], am: ["»"], eventName: "Digit9" },
-  { num: "0", ar: [], fr: ["à"], sp: [], am: ["օ"], eventName: "Digit0" },
+  { num: "`", ru: [], ar: [], fr: ["#"], am: [""], eventName: "Backquote" },
+  { num: "1", ru: [], ar: [], fr: ["&"], am: ["։"], eventName: "Digit1" },
+  { num: "2", ru: [], ar: [], fr: ["é"], am: ["ձ"], eventName: "Digit2" },
+  { num: "3", ru: [], ar: [], fr: [""], am: ["յ"], eventName: "Digit3" },
+  { num: "4", ru: [], ar: [], fr: ["'"], am: ["՛"], eventName: "Digit4" },
+  { num: "5", ru: [], ar: [], fr: [""], am: [","], eventName: "Digit5" },
+  { num: "6", ru: [], ar: [], fr: ["§"], am: ["-"], eventName: "Digit6" },
+  { num: "7", ru: [], ar: [], fr: ["è"], am: ["."], eventName: "Digit7" },
+  { num: "8", ru: [], ar: [], fr: ["!"], am: ["«"], eventName: "Digit8" },
+  { num: "9", ru: [], ar: [], fr: ["ç"], am: ["»"], eventName: "Digit9" },
+  { num: "0", ru: [], ar: [], fr: ["à"], am: ["օ"], eventName: "Digit0" },
 ];
 
 export const numbersWindowsLayout = [
-  { num: "`", ar: [" ّ", "ذ"], fr: ["²"], sp: ["º"], am: [], eventName: "Backquote" },
-  { num: "1", ar: [], fr: ["&"], sp: [], am: ["է"], eventName: "Digit1" },
-  { num: "2", ar: [], fr: ["é"], sp: [], am: ["թ"], eventName: "Digit2" },
-  { num: "3", ar: [], fr: [""], sp: [], am: ["փ"], eventName: "Digit3" },
-  { num: "4", ar: [], fr: ["'"], sp: [], am: ["ձ"], eventName: "Digit4" },
-  { num: "5", ar: [], fr: [""], sp: [], am: ["ջ"], eventName: "Digit5" },
-  { num: "6", ar: [], fr: ["-"], sp: [], am: ["ւ"], eventName: "Digit6" },
-  { num: "7", ar: [], fr: ["è"], sp: [], am: ["և"], eventName: "Digit7" },
-  { num: "8", ar: [], fr: ["_"], sp: [], am: ["ր"], eventName: "Digit8" },
-  { num: "9", ar: [], fr: ["ç"], sp: [], am: ["չ"], eventName: "Digit9" },
-  { num: "0", ar: [], fr: ["à"], sp: [], am: ["ճ"], eventName: "Digit0" },
-  { num: "-", ar: [], fr: [], sp: ["¿"], am: [], eventName: "Minus" },
-  { num: "=", ar: [], fr: [], sp: ["?"], am: ["ժ"], eventName: "Equal" },
+  { num: "`", ar: [" ّ", "ذ"], fr: ["²"], am: [], eventName: "Backquote" },
+  { num: "1", ar: [], fr: ["&"], am: ["է"], eventName: "Digit1" },
+  { num: "2", ar: [], fr: ["é"], am: ["թ"], eventName: "Digit2" },
+  { num: "3", ar: [], fr: [""], am: ["փ"], eventName: "Digit3" },
+  { num: "4", ar: [], fr: ["'"], am: ["ձ"], eventName: "Digit4" },
+  { num: "5", ar: [], fr: [""], am: ["ջ"], eventName: "Digit5" },
+  { num: "6", ar: [], fr: ["-"], am: ["ւ"], eventName: "Digit6" },
+  { num: "7", ar: [], fr: ["è"], am: ["և"], eventName: "Digit7" },
+  { num: "8", ar: [], fr: ["_"], am: ["ր"], eventName: "Digit8" },
+  { num: "9", ar: [], fr: ["ç"], am: ["չ"], eventName: "Digit9" },
+  { num: "0", ar: [], fr: ["à"], am: ["ճ"], eventName: "Digit0" },
+  { num: "-", ar: [], fr: [], am: [], eventName: "Minus" },
+  { num: "=", ar: [], fr: [], am: ["ժ"], eventName: "Equal" },
 ];
 
 export const macLayout = [
   [
-    { en: "Q", ar: [" َ", "ض"], fr: ["", "A"], sp: ["", "Q"], am: ["", "խ"], eventName: "KeyQ" },
-    { en: "W", ar: [" ً", "ص"], fr: ["", "Z"], sp: ["", "W"], am: ["", "վ"], eventName: "KeyW" },
-    { en: "E", ar: [" ِ", "ث"], fr: ["", "E"], sp: ["", "E"], am: ["", "է"], eventName: "KeyE" },
-    { en: "R", ar: [" ٍ", "ق"], fr: ["", "R"], sp: ["", "R"], am: ["", "ր"], eventName: "KeyR" },
-    { en: "T", ar: [" ُ", "ف"], fr: ["", "T"], sp: ["", "T"], am: ["", "դ"], eventName: "KeyT" },
-    { en: "Y", ar: [" ٌ", "غ"], fr: ["", "Y"], sp: ["", "Y"], am: ["", "ե"], eventName: "KeyY" },
-    { en: "U", ar: [" ْ", "ع"], fr: ["", "U"], sp: ["", "U"], am: ["", "ը"], eventName: "KeyU" },
-    { en: "I", ar: [" ّ", "ه"], fr: ["", "I"], sp: ["", "I"], am: ["", "ի"], eventName: "KeyI" },
-    { en: "O", ar: ["", "خ"], fr: ["", "O"], sp: ["", "O"], am: ["", "ո"], eventName: "KeyO" },
-    { en: "P", ar: ["", "ح"], fr: ["", "P"], sp: ["", "P"], am: ["", "բ"], eventName: "KeyP" },
-    { en: "{", ar: ["", "ج"], fr: ["", "^"], sp: ["", "´"], am: ["", "չ"], eventName: "BracketLeft" },
-    { en: "}", ar: ["", "ة"], fr: ["", "$"], sp: ["", "+"], am: ["", "ջ"], eventName: "BracketRight" },
+    { en: "Q", ru: ["", "й"], ar: [" َ", "ض"], fr: ["", "A"], am: ["", "խ"], eventName: "KeyQ" },
+    { en: "W", ru: ["", "ц"], ar: [" ً", "ص"], fr: ["", "Z"], am: ["", "վ"], eventName: "KeyW" },
+    { en: "E", ru: ["", "у"], ar: [" ِ", "ث"], fr: ["", "E"], am: ["", "է"], eventName: "KeyE" },
+    { en: "R", ru: ["", "к"], ar: [" ٍ", "ق"], fr: ["", "R"], am: ["", "ր"], eventName: "KeyR" },
+    { en: "T", ru: ["", "е"], ar: [" ُ", "ف"], fr: ["", "T"], am: ["", "դ"], eventName: "KeyT" },
+    { en: "Y", ru: ["", "н"], ar: [" ٌ", "غ"], fr: ["", "Y"], am: ["", "ե"], eventName: "KeyY" },
+    { en: "U", ru: ["", "г"], ar: [" ْ", "ع"], fr: ["", "U"], am: ["", "ը"], eventName: "KeyU" },
+    { en: "I", ru: ["", "ш"], ar: [" ّ", "ه"], fr: ["", "I"], am: ["", "ի"], eventName: "KeyI" },
+    { en: "O", ru: ["", "щ"], ar: ["", "خ"], fr: ["", "O"], am: ["", "ո"], eventName: "KeyO" },
+    { en: "P", ru: ["", "з"], ar: ["", "ح"], fr: ["", "P"], am: ["", "բ"], eventName: "KeyP" },
+    { en: "{", ru: ["", "х"], ar: ["", "ج"], fr: ["", "^"], am: ["", "չ"], eventName: "BracketLeft" },
+    { en: "}", ru: ["", "ъ"], ar: ["", "ة"], fr: ["", "$"], am: ["", "ջ"], eventName: "BracketRight" },
   ],
   [
-    { en: "A", ar: ["", "ش"], fr: ["", "Q"], sp: ["", "A"], am: ["", "ա"], eventName: "KeyA" },
-    { en: "S", ar: ["", "س"], fr: ["", "S"], sp: ["", "S"], am: ["", "ս"], eventName: "KeyS" },
-    { en: "D", ar: ["ى", "ي"], fr: ["", "D"], sp: ["", "D"], am: ["", "տ"], eventName: "KeyD" },
-    { en: "F", ar: ["", "ب"], fr: ["", "F"], sp: ["", "F"], am: ["", "ֆ"], eventName: "KeyF" },
-    { en: "G", ar: ["", "ل"], fr: ["", "G"], sp: ["", "G"], am: ["", "կ"], eventName: "KeyG" },
-    { en: "H", ar: ["آ", "ا"], fr: ["", "H"], sp: ["", "H"], am: ["", "հ"], eventName: "KeyH" },
-    { en: "J", ar: ["", "ت"], fr: ["", "J"], sp: ["", "J"], am: ["", "ճ"], eventName: "KeyJ" },
-    { en: "K", ar: ["", "ن"], fr: ["", "K"], sp: ["", "K"], am: ["", "ք"], eventName: "KeyK" },
-    { en: "L", ar: ["", "م"], fr: ["", "L"], sp: ["", "L"], am: ["", "լ"], eventName: "KeyL" },
-    { en: ";", ar: ["", "ك"], fr: ["", "M"], sp: ["", "ñ"], am: ["", "թ"], eventName: "Semicolon" },
-    { en: "\u200B", ar: ["", "؛"], fr: ["%", "ù"], sp: ["", "{"], am: ["", "փ"], eventName: "Quote" },
+    { en: "A", ru: ["", "ф"], ar: ["", "ش"], fr: ["", "Q"], am: ["", "ա"], eventName: "KeyA" },
+    { en: "S", ru: ["", "ы"], ar: ["", "س"], fr: ["", "S"], am: ["", "ս"], eventName: "KeyS" },
+    { en: "D", ru: ["", "в"], ar: ["ى", "ي"], fr: ["", "D"], am: ["", "տ"], eventName: "KeyD" },
+    { en: "F", ru: ["", "а"], ar: ["", "ب"], fr: ["", "F"], am: ["", "ֆ"], eventName: "KeyF" },
+    { en: "G", ru: ["", "п"], ar: ["", "ل"], fr: ["", "G"], am: ["", "կ"], eventName: "KeyG" },
+    { en: "H", ru: ["", "р"], ar: ["آ", "ا"], fr: ["", "H"],am: ["", "հ"], eventName: "KeyH" },
+    { en: "J", ru: ["", "о"], ar: ["", "ت"], fr: ["", "J"], am: ["", "ճ"], eventName: "KeyJ" },
+    { en: "K", ru: ["", "л"], ar: ["", "ن"], fr: ["", "K"], am: ["", "ք"], eventName: "KeyK" },
+    { en: "L", ru: ["", "д"], ar: ["", "م"], fr: ["", "L"], am: ["", "լ"], eventName: "KeyL" },
+    { en: ";", ru: ["", "ж"], ar: ["", "ك"], fr: ["", "M"], am: ["", "թ"], eventName: "Semicolon" },
+    { en: "\u200B", ru: ["", "э"], ar: ["", "؛"], fr: ["%", "ù"], am: ["", "փ"], eventName: "Quote" },
   ],
   [
-    { en: "Z", ar: ["", "ظ"], fr: ["", "W"], sp: ["", "Z"], am: ["", "զ"], eventName: "KeyZ" },
-    { en: "X", ar: ["", "ط"], fr: ["", "X"], sp: ["", "X"], am: ["", "ց"], eventName: "KeyX" },
-    { en: "C", ar: ["ئ", "ذ"], fr: ["", "C"], sp: ["", "C"], am: ["", "գ"], eventName: "KeyC" },
-    { en: "V", ar: ["ء", "د"], fr: ["", "V"], sp: ["", "V"], am: ["", "ւ"], eventName: "KeyV" },
-    { en: "B", ar: ["أ", "ز"], fr: ["", "B"], sp: ["", "B"], am: ["", "պ"], eventName: "KeyB" },
-    { en: "N", ar: ["إ", "ر"], fr: ["", "N"], sp: ["", "N"], am: ["", "ն"], eventName: "KeyN" },
-    { en: "M", ar: ["ؤ", "و"], fr: ["?", ","], sp: ["", "M"], am: ["", "մ"], eventName: "KeyM" },
-    { en: ",", ar: ["", "،"], fr: ["", ";"], sp: ["", ","], am: ["", "շ"], eventName: "Comma" },
-    { en: ".", ar: ["", "."], fr: ["/", ":"], sp: ["", "."], am: ["", "ղ"], eventName: "Period" },
-    { en: "/", ar: ["", "/"], fr: ["+", "="], sp: ["", "-"], am: ["", "ծ"], eventName: "Slash" },
+    { en: "Z", ru: ["", "я"], ar: ["", "ظ"], fr: ["", "W"], am: ["", "զ"], eventName: "KeyZ" },
+    { en: "X", ru: ["", "ч"], ar: ["", "ط"], fr: ["", "X"], am: ["", "ց"], eventName: "KeyX" },
+    { en: "C", ru: ["", "с"], ar: ["ئ", "ذ"], fr: ["", "C"], am: ["", "գ"], eventName: "KeyC" },
+    { en: "V", ru: ["", "м"], ar: ["ء", "د"], fr: ["", "V"], am: ["", "ւ"], eventName: "KeyV" },
+    { en: "B", ru: ["", "и"], ar: ["أ", "ز"], fr: ["", "B"], am: ["", "պ"], eventName: "KeyB" },
+    { en: "N", ru: ["", "т"], ar: ["إ", "ر"], fr: ["", "N"], am: ["", "ն"], eventName: "KeyN" },
+    { en: "M", ru: ["", "ь"], ar: ["ؤ", "و"], fr: ["?", ","], am: ["", "մ"], eventName: "KeyM" },
+    { en: ",", ru: ["", "б"], ar: ["", "،"], fr: ["", ";"], am: ["", "շ"], eventName: "Comma" },
+    { en: ".", ru: ["", "ю"], ar: ["", "."], fr: ["/", ":"], am: ["", "ղ"], eventName: "Period" },
+    { en: "/", ru: ["", "/"], ar: ["", "/"], fr: ["+", "="], am: ["", "ծ"], eventName: "Slash" },
   ],
 ];
 
 export const windowsLayout = [
   [
-    { en: "Q", ar: ["َ", "ض"], fr: ["", "A"], sp: ["", "Q"], am: ["", "ք"], eventName: "KeyQ" }, // Fatha
-    { en: "W", ar: ["ً", "ص"], fr: ["", "Z"], sp: ["", "W"], am: ["", "ո"], eventName: "KeyW" }, // Tanwin Fatha
-    { en: "E", ar: ["ُ", "ث"], fr: ["", "E"], sp: ["", "E"], am: ["", "ե"], eventName: "KeyE" }, // Damma
-    { en: "R", ar: ["ٌ", "ق"], fr: ["", "R"], sp: ["", "R"], am: ["", "ռ"], eventName: "KeyR" }, // Tanwin Damma
-    { en: "T", ar: ["لإ", "ف"], fr: ["", "T"], sp: ["", "T"], am: ["", "տ"], eventName: "KeyT" }, // Kasra
-    { en: "Y", ar: ["إ", "غ"], fr: ["", "Y"], sp: ["", "Y"], am: ["", "ը"], eventName: "KeyY" }, // Tanwin Kasra
-    { en: "U", ar: ["‘", "ع"], fr: ["", "U"], sp: ["", "U"], am: ["", "ւ"], eventName: "KeyU" }, // Sukun
-    { en: "I", ar: ["", "ه"], fr: ["", "I"], sp: ["", "I"], am: ["", "ի"], eventName: "KeyI" },
-    { en: "O", ar: ["", "خ"], fr: ["", "O"], sp: ["", "O"], am: ["", "օ"], eventName: "KeyO" },
-    { en: "P", ar: ["", "ح"], fr: ["", "P"], sp: ["", "P"], am: ["", "պ"], eventName: "KeyP" },
-    { en: "[", ar: ["", "ج"], fr: ["¨", "^"], sp: ["", "`"], am: ["", "խ"], eventName: "BracketLeft" },
-    { en: "]", ar: ["", "د"], fr: ["£", "$"], sp: ["", "+"], am: ["", "ծ"], eventName: "BracketRight" },
-    { en: "\\", ar: ["", ""], fr: ["", ""], sp: ["", ""], am: ["", "շ"], eventName: "Backslash" },    
+    { en: "Q", ar: ["َ", "ض"], fr: ["", "A"], am: ["", "ք"], eventName: "KeyQ" }, // Fatha
+    { en: "W", ar: ["ً", "ص"], fr: ["", "Z"], am: ["", "ո"], eventName: "KeyW" }, // Tanwin Fatha
+    { en: "E", ar: ["ُ", "ث"], fr: ["", "E"], am: ["", "ե"], eventName: "KeyE" }, // Damma
+    { en: "R", ar: ["ٌ", "ق"], fr: ["", "R"], am: ["", "ռ"], eventName: "KeyR" }, // Tanwin Damma
+    { en: "T", ar: ["لإ", "ف"], fr: ["", "T"], am: ["", "տ"], eventName: "KeyT" }, // Kasra
+    { en: "Y", ar: ["إ", "غ"], fr: ["", "Y"], am: ["", "ը"], eventName: "KeyY" }, // Tanwin Kasra
+    { en: "U", ar: ["‘", "ع"], fr: ["", "U"], am: ["", "ւ"], eventName: "KeyU" }, // Sukun
+    { en: "I", ar: ["", "ه"], fr: ["", "I"], am: ["", "ի"], eventName: "KeyI" },
+    { en: "O", ar: ["", "خ"], fr: ["", "O"], am: ["", "օ"], eventName: "KeyO" },
+    { en: "P", ar: ["", "ح"], fr: ["", "P"], am: ["", "պ"], eventName: "KeyP" },
+    { en: "[", ar: ["", "ج"], fr: ["¨", "^"], am: ["", "խ"], eventName: "BracketLeft" },
+    { en: "]", ar: ["", "د"], fr: ["£", "$"], am: ["", "ծ"], eventName: "BracketRight" },
+    { en: "\\", ar: ["", ""], fr: ["", ""], am: ["", "շ"], eventName: "Backslash" },    
   ],
   [
-    { en: "A", ar: [" ِ", "ش"], fr: ["", "Q"], sp: ["", "A"], am: ["", "ա"], eventName: "KeyA" }, // Tatwil
-    { en: "S", ar: [" ٍ", "س"], fr: ["", "S"], sp: ["", "S"], am: ["", "ս"], eventName: "KeyS" },
-    { en: "D", ar: ["", "ي"], fr: ["", "D"], sp: ["", "D"], am: ["", "դ"], eventName: "KeyD" },
-    { en: "F", ar: ["", "ب"], fr: ["", "F"], sp: ["", "F"], am: ["", "ֆ"], eventName: "KeyF" },
-    { en: "G", ar: ["لأ", "ل"], fr: ["", "G"], sp: ["", "G"], am: ["", "գ"], eventName: "KeyG" },
-    { en: "H", ar: ["أ", "ا"], fr: ["", "H"], sp: ["", "H"], am: ["", "հ"], eventName: "KeyH" },
-    { en: "J", ar: ["", "ت"], fr: ["", "J"], sp: ["", "J"], am: ["", "յ"], eventName: "KeyJ" },
-    { en: "K", ar: ["", "ن"], fr: ["", "K"], sp: ["", "K"], am: ["", "կ"], eventName: "KeyK" },
-    { en: "L", ar: ["", "م"], fr: ["", "L"], sp: ["", "L"], am: ["", "լ"], eventName: "KeyL" },
-    { en: ";", ar: ["", "ك"], fr: ["", "M"], sp: ["", "ñ"], am: ["", ";"], eventName: "Semicolon" }, // Shift doesn't change
-    { en: "'", ar: ["", "ط"], fr: ["%", "ù"], sp: ["", "´"], am: ["", "՛"], eventName: "Quote" }, // Same
+    { en: "A", ar: [" ِ", "ش"], fr: ["", "Q"], am: ["", "ա"], eventName: "KeyA" }, // Tatwil
+    { en: "S", ar: [" ٍ", "س"], fr: ["", "S"], am: ["", "ս"], eventName: "KeyS" },
+    { en: "D", ar: ["", "ي"], fr: ["", "D"], am: ["", "դ"], eventName: "KeyD" },
+    { en: "F", ar: ["", "ب"], fr: ["", "F"], am: ["", "ֆ"], eventName: "KeyF" },
+    { en: "G", ar: ["لأ", "ل"], fr: ["", "G"], am: ["", "գ"], eventName: "KeyG" },
+    { en: "H", ar: ["أ", "ا"], fr: ["", "H"], am: ["", "հ"], eventName: "KeyH" },
+    { en: "J", ar: ["", "ت"], fr: ["", "J"], am: ["", "յ"], eventName: "KeyJ" },
+    { en: "K", ar: ["", "ن"], fr: ["", "K"], am: ["", "կ"], eventName: "KeyK" },
+    { en: "L", ar: ["", "م"], fr: ["", "L"], am: ["", "լ"], eventName: "KeyL" },
+    { en: ";", ar: ["", "ك"], fr: ["", "M"], am: ["", ";"], eventName: "Semicolon" }, // Shift doesn't change
+    { en: "'", ar: ["", "ط"], fr: ["%", "ù"], am: ["", "՛"], eventName: "Quote" }, // Same
   ],
   [
-    { en: "Z", ar: ["", "ئ"], fr: ["", "W"], sp: ["", "Z"], am: ["", "զ"], eventName: "KeyZ" }, // Shifted seems to be Kasra (historically), but can be left blank
-    { en: "X", ar: [" ْ", "ء"], fr: ["", "X"], sp: ["", "X"], am: ["", "ղ"], eventName: "KeyX" },
-    { en: "C", ar: ["", "ؤ"], fr: ["", "C"], sp: ["", "C"], am: ["", "ց"], eventName: "KeyC" },
-    { en: "V", ar: ["", "ر"], fr: ["", "V"], sp: ["", "V"], am: ["", "վ"], eventName: "KeyV" },
-    { en: "B", ar: ["لآ", "لا"], fr: ["", "B"], sp: ["", "B"], am: ["", "բ"], eventName: "KeyB" },
-    { en: "N", ar: ["آ", "ى"], fr: ["", "N"], sp: ["", "N"], am: ["", "ն"], eventName: "KeyN" },
-    { en: "M", ar: ["", "ة"], fr: ["?", ","], sp: ["", "M"], am: ["", "մ"], eventName: "KeyM" },
-    { en: ",", ar: ["", "و"], fr: [".", ";"], sp: ["", ","], am: ["", ","], eventName: "Comma" },
-    { en: ".", ar: ["", "ز"], fr: ["/", ":"], sp: ["", "."], am: ["", ":"], eventName: "Period" },
-    { en: "/", ar: ["؟", "ظ"], fr: ["§", "!"], sp: ["", "-"], am: ["", "/"], eventName: "Slash" }, // Arabic question mark
+    { en: "Z", ar: ["", "ئ"], fr: ["", "W"], am: ["", "զ"], eventName: "KeyZ" }, // Shifted seems to be Kasra (historically), but can be left blank
+    { en: "X", ar: [" ْ", "ء"], fr: ["", "X"], am: ["", "ղ"], eventName: "KeyX" },
+    { en: "C", ar: ["", "ؤ"], fr: ["", "C"], am: ["", "ց"], eventName: "KeyC" },
+    { en: "V", ar: ["", "ر"], fr: ["", "V"], am: ["", "վ"], eventName: "KeyV" },
+    { en: "B", ar: ["لآ", "لا"], fr: ["", "B"], eventName: "KeyB" },
+    { en: "N", ar: ["آ", "ى"], fr: ["", "N"], am: ["", "ն"], eventName: "KeyN" },
+    { en: "M", ar: ["", "ة"], fr: ["?", ","], am: ["", "մ"], eventName: "KeyM" },
+    { en: ",", ar: ["", "و"], fr: [".", ";"], am: ["", ","], eventName: "Comma" },
+    { en: ".", ar: ["", "ز"], fr: ["/", ":"], am: ["", ":"], eventName: "Period" },
+    { en: "/", ar: ["؟", "ظ"], fr: ["§", "!"], am: ["", "/"], eventName: "Slash" }, // Arabic question mark
   ],
 ];
