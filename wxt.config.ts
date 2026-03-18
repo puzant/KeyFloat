c1a213a47a3b52b51cca091a9b2e99175e382903
Puzant <pbakjejian@hotmail.com>
2026-03-18 14:11:46 +0300

    bump version

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
     permissions: ['storage', 'scripting', 'tabs'],
-    version: '1.4.1',
+    version: '1.5.0',
     name: 'KeyFloat',
     description: 'Floating multilingual keyboard with native key mappings, drag, dark mode, sounds, and dynamic layouts for macOS & Windows',
     host_permissions: ['<all_urls>'],
